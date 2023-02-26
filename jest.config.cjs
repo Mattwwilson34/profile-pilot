d109944bf17d5f95c8981c7a1c4ef7028a43f484
Matt Wilson <mattwwilson34@gmail.com>
2023-02-26 12:54:37 -0500

    test(Jest): move jest config to seperate file

---
+++ jest.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'html', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'main.tsx',
+    'index.ts',
+    '/firebase/',
+  ],
+};
