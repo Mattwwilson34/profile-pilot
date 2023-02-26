01f2b5b20b98dda7339a7a645fe20930590a71e0
Matt Wilson <mattwwilson34@gmail.com>
2023-02-26 12:59:46 -0500

    test(Root): add unit testing

---
+++ src/__tests__/routes/Root.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { act, render, screen } from '@testing-library/react';
+import Root from '../../routes/Root';
+import { FirebaseAuthProvider } from '../../context/FireBaseAuthContext';
+import { type User } from 'firebase/auth';
+import useFirebaseAuth from '../../hooks/useFirebaseAuth';
+
+jest.mock('../../hooks/useFirebaseAuth', () => jest.fn());
+
+const renderRootRoute = async (
+  user: User | undefined = undefined
+): Promise<void> => {
+  await act(async () => {
+    render(
+      <FirebaseAuthProvider value={user}>
+        <BrowserRouter>
+          <Root />
+        </BrowserRouter>
+      </FirebaseAuthProvider>
+    );
+  });
+};
+
+describe('<Root />', () => {
+  it('renders component when user is not null or undefined', async () => {
+    useFirebaseAuth.mockReturnValue({ user: 'matt' });
+    await renderRootRoute();
+    const text = screen.getByText('Profile Pilot');
+    expect(text).toBeInTheDocument();
+  });
+
+  it('renders loading message if user is undefined', async () => {
+    useFirebaseAuth.mockReturnValue(undefined);
+    await renderRootRoute();
+    const text = screen.getByText('Loading user info');
+    expect(text).toBeInTheDocument();
+  });
+
+  it('renders login button if user is null', async () => {
+    useFirebaseAuth.mockReturnValue(null);
+    await renderRootRoute();
+    const text = screen.getByText('Login Please');
+    expect(text).toBeInTheDocument();
+  });
+});
