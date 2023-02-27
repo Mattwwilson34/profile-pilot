3cc7897d1774dd7716cbb6be1d98aecabea54b03
Matt Wilson <mattwwilson34@gmail.com>
2023-02-27 11:14:53 -0500

    style(theme): add MUI theme to app

---
+++ src/style/app-theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+export const appTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#0079BC',
+    },
+    secondary: {
+      main: '#f97070',
+    },
+  },
+});
