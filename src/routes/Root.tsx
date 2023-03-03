c379b3ae5d7b8536c4f6c7330e4b7a7fa6005477
Matt Wilson <mattwwilson34@gmail.com>
2023-03-02 22:17:12 -0500

    refactor(UserAuth): to use React Query over FirebaseAuthContext
    The google auth listener that is setup in the FirebaseAuthContext
    did not provide enough functionality for me to reliably get my
    user data into the components that I needed. This was realized when
    a user fills out the new registration survey. After this occurs I
    need to update the firestore database and then refetch the user from
    the firestore to make sure that the user state stays in sync with the
    application. React Query works very well with this because we can
    invalidate chosen queries which initiates an auto refetch. As a result
    I refactored the application to use React Query instead of the prior
    Auth Context provider.

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,46 +1,45 @@
 import React, { useEffect } from 'react';
-import { CircularProgress } from '@mui/material';
-import useFirebaseAuth from '../hooks/useFirebaseAuth';
 import TopNav from '../features/top-nav';
 import Login from '../features/login';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { getUserFromFirestoreById } from '../firebase/firebase-db';
+import { useQuery } from 'react-query';
 
 const Root = (): JSX.Element => {
-  const user = useFirebaseAuth();
   const navigate = useNavigate();
+  const localUserJson = localStorage.getItem('profile-pilot');
+  const localUser = JSON.parse(localUserJson ?? 'null');
 
   useEffect(() => {
-    if (user != null) {
-      if (user.surveyData == null) {
-        navigate('/survery');
-      } else {
-        navigate('/profile');
-      }
+    const surveyDataPresent = localUser?.surveyData;
+    if (surveyDataPresent == null) {
+      navigate('/survey');
+    } else if (typeof surveyDataPresent === 'object') {
+      navigate(`/profile/${localUser.docId as string}`);
     }
-  }, [user]);
+  }, []);
 
-  if (user === undefined) {
-    return (
-      <>
-        <TopNav user={user} />
-        <h1>Loading user info</h1>
-        <CircularProgress />
-      </>
-    );
+  const {
+    isLoading,
+    data: user,
+    isSuccess,
+  } = useQuery({
+    queryKey: 'user',
+    queryFn: async () => await getUserFromFirestoreById(localUser.docId),
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+
+  const isLoggedIn = user != null && isSuccess;
+
+  if (isLoading) {
+    return <div>Loading user data in the Root route</div>;
   }
 
-  if (user === null) {
-    return (
-      <>
-        <TopNav user={user} />
-        <Login />
-      </>
-    );
-  }
   return (
     <>
-      <TopNav user={user} />
-      <Outlet />
+      <TopNav userAuthorized={isLoggedIn} />
+      {isLoggedIn ? <Outlet /> : <Login />}
     </>
   );
 };
