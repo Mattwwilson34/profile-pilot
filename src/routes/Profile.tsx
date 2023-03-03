e16eb6324179292580dcc7cb891e9d32e57d6ebd
Matt Wilson <mattwwilson34@gmail.com>
2023-03-03 12:52:32 -0500

    feat(route): add Profile route

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import UserProfile from '../features/user-profile';
 import type { User } from '../types/User';
 
 const Profile = (): JSX.Element => {
-  const data = useLoaderData() as User;
-  return <div>{`Welcome ${data.username as string}`}</div>;
+  const userData = useLoaderData() as User;
+  return <UserProfile userData={userData} />;
 };
 
 export default Profile;
