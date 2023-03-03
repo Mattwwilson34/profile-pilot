9f40b4db2166c1eec84d0bdcd7ad66ae07fb42ba
Matt Wilson <mattwwilson34@gmail.com>
2023-03-02 22:14:17 -0500

    feat(logout): add handlLogout function with local storage handling

--- src/features/top-nav/components/TopNav.tsx
+++ src/features/top-nav/components/TopNav.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import { signOutOfFirebase } from '../../../firebase/firebase-auth';
-import type { User } from '../../../types/User';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { useQueryClient } from 'react-query';
 
 interface TopNavProps {
   userAuthorized: boolean | undefined | null;
 }
 
 const TopNav = ({ userAuthorized }: TopNavProps): JSX.Element => {
+  const queryClient = useQueryClient();
+
+  const handleLogout = (): void => {
+    void (async () => {
+      signOutOfFirebase();
+      await queryClient.invalidateQueries({ queryKey: ['user'] });
+    })();
+  };
+
   return (
     <Box sx={{ flexGrow: 1, marginBottom: 2 }}>
       <AppBar position='static'>
@@ -19,7 +28,6 @@ const TopNav = ({ userAuthorized }: TopNavProps): JSX.Element => {
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             Profile Pilot
           </Typography>
-            <Button color='inherit' onClick={signOutOfFirebase}>
           {userAuthorized === false ? null : (
             <Button color='inherit' onClick={handleLogout}>
               Logout
