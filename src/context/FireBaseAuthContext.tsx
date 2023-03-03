4003b479932ed5a0cd2e0095ecf3f9ed886297c0
Matt Wilson <mattwwilson34@gmail.com>
2023-03-02 22:01:52 -0500

    feat(context): add local storage and pending state

--- src/context/FireBaseAuthContext.tsx
+++ src/context/FireBaseAuthContext.tsx
@@ -24,24 +24,42 @@ const formatUserData = (googleUser: GoogleUser): User => {
   };
   return user;
 };
+
+const setUserInLocalStorage = (user: User): void => {
+  const userExistsInStorage = localStorage.getItem('profile-pilot');
+  if (userExistsInStorage === null) {
+    localStorage.setItem('profile-pilot', JSON.stringify(user));
+  }
+};
+
 const FirebaseAuthProvider: React.FC<FirebaseAuthProviderProps> = ({
   children,
 }: FirebaseAuthProviderProps): JSX.Element => {
   const [user, setUser] = useState<User | null>(null);
+  const [pending, setPending] = useState(true);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(
       (googleUser: GoogleUser | null) => {
+        if (googleUser === null) {
+          setPending(false);
+          return;
+        }
         void (async () => {
           const formatedUser = formatUserData(googleUser);
           await addNewUserToFirestore(formatedUser);
           setUserInLocalStorage(formatedUser);
           setUser(formatedUser);
+          setPending(false);
         })();
       }
     );
     return unsubscribe;
   }, []);
+
+  if (pending) {
+    return <div>Waiting for user data to load in Context.</div>;
+  }
 
   return (
     <FirebaseAuthContext.Provider value={{ user }}>
